57189a822010b3472ff7725f327a4eda7923e067
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-12-05 08:35:34 +0100

    ADD card style

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/prop-types */
+import columnStyle from './Column.module.css'
+
 export default function Column({title}) {
     return(
         <>
         <div className="col">
-            <div className="card">
-                <div className="card-title">{title}</div>
-                <div className="card-body">
+            <div className={columnStyle.card}>
+                <div className={columnStyle.title}>{title}</div>
+                <div className={columnStyle.body}>
                     <div>fare
                     </div>
                 </div>
